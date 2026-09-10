f6e9a11f378cb80c38aedf7277ffc2e22b15a30e
THAYSSA APARECIDA PAULO E SILVA VIANA <thayssa_viana@estudante.sesisenai.org.br>
2026-09-10 09:25:52 -0300

    Adicionei a função de listar pedidos ao controller e ajeitei a funçãode criar pedido (o push tava antes do return).

--- src/controllers/pedidosController.js
+++ src/controllers/pedidosController.js
@@ -3,14 +3,26 @@ import { pedidos } from "pedidosModel";
 export async function criarPedido (req, res) {
     try {
         const pedido = await pedidoService.criar(req.body);
+        pedidos.push(pedido)
         return res.status(201).json({
             mensagem: "O pedido foi criado com sucesso.",
             pedido: pedido
         });
-        pedidos.push(pedido)
+        
     } catch (erro) {
         return res.status(400).json({
             mensagem: "Erro ao criar pedido.",
+            erro: erro.message
+        });
+    }
+}
+
+export async function listarPedidos(req, res) {
+    try {
+        return res.status(200).json(pedidos);
+    } catch (erro) {
+        return res.status(400).json({
+            mensagem: "Erro ao listar pedidos.",
             erro: erro.message
         });
     }
